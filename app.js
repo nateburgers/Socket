5979c950236e97a7bd7cf975326ea70dcaa0543e
Nathan Burgers <nateburgers@gmail.com>
2013-11-10 07:05:44 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+var express = require('express');
+var http = require('http');
+var jade = require('jade');
+var app = express();
+var server = http.createServer(app)
+
+app.set('views', __dirname + "/views");
+app.set('view engine', 'jade');
+app.set('view options', {layout: false});
+app.configure(function(){
+    app.use(express.static(__dirname+"/public"))
+});
+app.get("/", function(req, res) {
+    res.render('index.jade')
+});
+app.listen(3000);
+
+var io = require('socket.io').listen(server);
+io.sockets.on('connection', function(socket){
+    socket.on('setPsuedo', function(data) {
+	socket.set('psuedo', data);
+    });
+
+    socket.on('message', function(message) {
+	socket.get('psuedo', function(error, name) {
+	    var data = { 'message': message, psuedo : name };
+	    socket.broadcast.emit('message', data);
+	    console.log("user")
+	})
+    })
+})
